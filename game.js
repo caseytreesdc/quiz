04ffe9879a368bbe0145e0a7b13ab603a43d1106
Alison Kim <akim@caseytrees.org>
2019-10-18 11:57:29 -0400

    next question button

--- game.js
+++ game.js
@@ -7,7 +7,8 @@ const scoreText = document.getElementById('score');
 const nextquestion = document.getElementById("nextquestion");
 const choicecontainer = document.getElementsByClassName("choice-container");
 const progressBarFull = document.getElementById("progressBarFull");
-//const answerExplanation = document.getElementById("answer-explanation");
+
+console.log(choices, choices[3].parentElement);
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -18,18 +19,18 @@ let availableQuestions = [];
 let questions = [
     {
     questionImage: "<img src='images/trees.png' width='100%'>",
-    question: "How many trees did our volunteers plant this spring?",
-    explanation: "Our volunteers planted 3000 trees last spring",
-    choice1: "100",
-    choice2: "500", 
-    choice3: "1000",
-    choice4: "3000",
+    question: "How many trees were collectively planted in the District in 2018?",
+    explanation: "For the city to achieve its 40 percent canopy goal by 2032, at least 10,648 trees must be planted each year for the next 14 years. In 2018, 15,529 trees were collectively planted throughout D.C. This vastly surpasses the target and is the highest planting number to date",
+    choice1: "5,479",
+    choice2: "10,648", 
+    choice3: "13,456",
+    choice4: "15,529",
     answer: 4
     },
     {
     questionImage: "<img src='images/volunteers.png' width='100%'>",
     question: "How many people volunteered with Casey Trees in the past year?",
-    explanation: "1203 volunteers joined us to plant, inventory and care for D.C.'s urban forest", 
+    explanation: "We love our volunteers. Story about how great our volunteers are. Why you should become one. 1203 volunteers joined us to plant, inventory and care for D.C.'s urban forest.", 
     choice1: "202",
     choice2: "423", 
     choice3: "1545",
@@ -39,7 +40,7 @@ let questions = [
     {
     questionImage: "<img src='images/water.png' width='100%'>",
     question: "How much water do newly planted trees need to survive per week?",
-    explanation: "Trees need 25 gallons of water per week to survive",
+    explanation: "The single most important factor for young tree survival is water. Continue watering through the first two to three years after planting. Young trees require 1.5 inches of rain or 25 gallons of water per week for the first three years to establish their roots.",
     choice1: "2 gallons",
     choice2: "2.5 gallons", 
     choice3: "25 gallons",
@@ -49,7 +50,7 @@ let questions = [
     {
     questionImage: "<img src='images/sweetgum.png' width='100%'>",
     question: "How many pounds of carbon can a mature sweetgum remove from the atmosphere?",
-    explanation: "A mature sweetgum can remove 464 pounds of carbon from the atmosphere",
+    explanation: "The American sweetgum has star-shaped leaves and spikey fruit valued by urban wildlife. A mature sweetgum can remove 464 pounds of carbon from the atmosphere and intercept approx. 4,389 gallons of stormwater runoff in a year.",
     choice1: "568 pounds",
     choice2: "464 pounds", 
     choice3: "305 pounds",
@@ -107,8 +108,9 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset["number"];
         const correctAnswer = currentQuestion.answer;
 
+        console.log(choices, choices[correctAnswer-1].parentElement);
         console.log(correctAnswer);
-
+        
         const classToApply = 
             selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
@@ -117,31 +119,39 @@ choices.forEach(choice => {
             }
 
             if (classToApply === "incorrect") {
-                progressBarFull.parentElement.classList.add("correct");
+                choices[correctAnswer-1].parentElement.classList.add("correct");
             }
 
             console.log(questionImage.innerText);
-
+          
 
         selectedChoice.parentElement.classList.add(classToApply);
         explanation.classList.remove("correct-answer-hidden");
         explanation.classList.add("correct-answer-showing");
 
         setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-           explanation.classList.remove("correct-answer-showing");
-            explanation.classList.add("correct-answer-hidden");
-            getNewQuestion();
+           //selectedChoice.parentElement.classList.remove(classToApply);
+           //choices[correctAnswer-1].parentElement.classList.remove("correct");
+           //explanation.classList.remove("correct-answer-showing");
+           //explanation.classList.add("correct-answer-hidden");
+            //getNewQuestion();
         }, 2000)
 
         
     });
 });
 
-/*nextquestion.addEventListener("click", e => {
+nextquestion.addEventListener("click", e => {
     getNewQuestion();
-    choicecontainer.classList.remove(classToApply);     
-});*/
+
+    explanation.classList.remove("correct-answer-showing");
+    explanation.classList.add("correct-answer-hidden");
+        
+    for (var i = 0; i < choices.length; i++) {
+            choices[i].parentElement.classList.remove("correct", "incorrect");
+        }
+    
+});
 
 
 incrementScore = num => {
