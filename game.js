ad51f1e5d0dc892b2950dcec3f1fd046d06f43a6
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-04-06 09:45:49 -0400

    updated images for arbor day questions

--- game.js
+++ game.js
@@ -18,7 +18,7 @@ let availableQuestions = [];
 
 let questions = [
     {
-    questionImage: "<img src='images/morton_and_caroline.jpg' width='100%'>",
+    questionImage: "<img alt='Caroline Ann Joy French' src='images/caroline.jpg' width='100%'>",
     question: "To what state did Julius Sterling Morton and his wife Caroline Joy French move to where he even briefly served as Acting Governor?",
     explanation: "They were shocked by the lack of trees to help build and heat their home, as well as the lack of fruit and ornamental trees that provided entertainment and beauty. Photo Courtesy of NE State Historical Society",
     choice1: "South Dakota",
@@ -28,7 +28,7 @@ let questions = [
     answer: 2
     },
     {
-    questionImage: "<img src='images/scarletoak.png' width='100%'>",
+    questionImage: "<img alt='arbor dagy celebrations' src='images/arborday.jpg' width='100%'>",
     question: "In what year did Morton propose a resolution where his entire state would set aside one day in which they would plant trees?",
     explanation: "Birdsey Northrop of Connecticut was responsible for globalizing the idea when he visited Japan in 1883 and delivered his Arbor Day and Village Improvement message. In that same year, the American Forestry Association made Northrop the Chairman of the committee to campaign for Arbor Day nationwide. He also brought his enthusiasm for Arbor Day to Australia, Canada, and Europe.", 
     choice1: "1872",
@@ -38,17 +38,17 @@ let questions = [
     answer: 1, 
     },
     {
-    questionImage: "<img src='images/water.png' width='100%'>",
+    questionImage: "<img alt='' src='images/trees.png' width='100%'>",
     question: "Why was April 22 originally chosen as the official holiday for Arbor Day?",
     explanation: "Today, many countries observe such a holiday. Though usually observed in the spring, the date varies, depending on climate and suitable planting season.",
     choice1: "It’s easy to remember the number 22",
     choice2: "It was on a Saturday", 
     choice3: "It was J. Sterling Morton’s birthday",
     choice4: "State arborists suggested it",
-    answer: 2
+    answer: 3
     },
     {
-    questionImage: "<img src='images/sweetgum.png' width='100%'>",
+    questionImage: "<img src='images/epa.jpg' width='100%'>",
     question: "By 1920, how many states and territorial possessions of the U.S. were celebrating Arbor Day?",
     explanation: "On the first Arbor Day, April 10, 1872, an estimated one million trees were planted",
     choice1: "25",
@@ -58,7 +58,7 @@ let questions = [
     answer: 4
     },
     {
-    questionImage: "<img src='images/sweetgum.png' width='100%'>",
+    questionImage: "<img src='images/epa-logo-2.jpg' width='100%'>",
     question: "Which President recognized Arbor Day nationwide as part of his administration’s other environmentally friendly actions?",
     explanation: "These included establishing the EPA.",
     choice1: "Richard Nixon",
